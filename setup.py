ae45064bfefafe8754490cbf68a33d015289b183
ssut <ssut@ssut.me>
2015-06-16 22:02:28 +0900

    Add setup.py for PyPi distribution

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+def install():
+    desc = 'A Python client library for nyaa.se!',
+    setup(
+        name='py-nyaa',
+        version='1.0',
+        description=desc,
+        long_description=desc,
+        author='SuHun Han',
+        author_email='ssut@ssut.me',
+        url='https://github.com/ssut/py-nyaa',
+        classifiers = ['Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Education',
+            'Intended Audience :: End Users/Desktop',
+            'License :: Freeware',
+            'Operating System :: POSIX',
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: MacOS :: MacOS X',
+            'Topic :: Education',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4'],
+        packages=find_packages(),
+        install_requires=[
+            'requests',
+            'xmltodict',
+        ],
+    )
+
+if __name__ == "__main__":
+    install()
