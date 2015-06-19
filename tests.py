48b6b97da349c8e4c5ba4ef27bd58df91fa3a9a2
ssut <ssut@ssut.me>
2015-06-19 23:16:41 +0900

    Update test cases

--- tests.py
+++ tests.py
@@ -8,13 +8,15 @@ class NyaaTests(unittest.TestCase):
     def test_basic(self):
         results = [
             nyaa.search()[0],
-            nyaa.search('horrible subs', 2, '-seeders')[0],
-            nyaa.search('horrible subs', 1, 'seeders')[0],
+            nyaa.search('horrible subs', 2, order='-seeders')[0],
+            nyaa.search('horrible subs', 1, order='seeders')[0],
+            nyaa.search('fate stay night', 1, user=64513)[0],
         ]
 
         assert results[0].title != ''
         assert results[1].seeders > 0
         assert results[2].seeders == 0
+        assert results[3].title != ''
 
 if __name__ == '__main__':
     unittest.main()
